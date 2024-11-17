2e9844b173f433be56ff34b52569c16b0d8f4f5c
JohnFrancio <franciaw@gmail.com>
2024-11-17 09:37:18 +0300

    add assets

--- src/components/InfoBackground.jsx
+++ src/components/InfoBackground.jsx
@@ -10,7 +10,7 @@ function InfoBackground({ position }) {
         <p>Hello, I'm <span className="font-poppins text-2xl animate-pulse">John Francio</span>, a Frontend JavaScript Developer</p>
         <p>Use the arrows to check out my projects as you explore space!</p>
         <p>Lets GO!!!</p>
-        <img className="absolute -right-8 -top-24 md:-right-4 md:-top-28 rounded-full border-2 border-white w-32 h-32 md:w-60 md:h-60 object-cover" src="./src/assets/me.jpg" alt="me" />
+        <img className="absolute -right-8 -top-24 md:-right-4 md:-top-28 rounded-full border-2 border-white w-32 h-32 md:w-60 md:h-60 object-cover" src="/assets/me.jpg" alt="me" />
       </div>
     </div>
   );
