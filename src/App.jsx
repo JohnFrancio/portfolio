27989677c0c24217f83a38c0cebb3baeaa11e8fd
JohnFrancio <franciaw@gmail.com>
2024-11-17 09:57:04 +0300

    speed of slide & arrow position

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
 
     const moveShip = () => {
       setPosition((prev) => {
-        const newPos = prev + direction * 0.03; // Adjust speed (0.01) for smooth movement
+        const newPos = prev + direction * 0.02; // Adjust speed (0.01) for smooth movement
         return Math.max(0, Math.min(newPos, 10)); // Keep position between 0 and 3 (or adjust based on the number of sections)
       });
       animationFrame = requestAnimationFrame(moveShip);
@@ -47,7 +47,7 @@ function App() {
         {/* <OrbitControls/> */}
       </Canvas>
       <InfoBackground position={position} />
-      <div className="absolute bottom-10 left-[38%] md:bottom-[20px] md:left-[20px] flex items-center justify-center gap-5">
+      <div className="absolute bottom-20 left-[38%] md:bottom-[20px] md:left-[20px] flex items-center justify-center gap-5">
         <button onClick={handleLeftClick}><FaArrowLeft className="fill-white" size={32}/></button>
         <button onClick={handleRightClick}><FaArrowRight className="fill-white" size={32}/></button>
       </div>
